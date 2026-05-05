aacc2b3e29915c10a6df34f44b8dad48516e98f1
Marta Solomacha <marciamarcia@o2.pl>
2026-05-05 17:21:44 +0200

    Add lessons pages and handle progress

---
+++ app/components/LessonCompleteButton.client.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+defineProps<{
+  modelValue: boolean;
+}>();
+
+defineEmits<{
+  (e: "update:modelValue", value: boolean): void;
+}>();
+</script>
+
+<template>
+  <label
+    class="rounded text-white font-bold py-2 px-4 cursor-pointer"
+    :class="{
+      'bg-green-500': modelValue,
+      'bg-gray-500': !modelValue,
+    }"
+  >
+    <input
+      type="checkbox"
+      class="hidden"
+      :value="modelValue"
+      @input="() => $emit('update:modelValue', !modelValue)"
+    />
+    {{ modelValue ? "Completed!" : "Mark as complete" }}
+  </label>
+</template>
+
+<style scoped>
+@reference "~/assets/css/tailwind.css";
+
+::selection {
+  display: none;
+}
+</style>
