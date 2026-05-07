1916d679c14900c58e0f5347f2910a777d3d34a9
Marta Solomacha <marciamarcia@o2.pl>
2026-05-07 15:08:43 +0200

    Add client side error handling

--- app/composables/useCourse.ts
+++ app/composables/useCourse.ts
@@ -1,14 +1,43 @@
 import courseData from "./courseData";
 
-export const useCourse = () => {
+type Lesson = {
+  title: string;
+  slug: string;
+  number: number;
+  downloadUrl: string;
+  sourceUrl?: string;
+  videoId: number;
+  text: string;
+  path: string;
+};
+
+type Chapter = {
+  title: string;
+  slug: string;
+  number: number;
+  lessons: Lesson[];
+};
+
+type Course = {
+  title: string;
+  chapters: Chapter[];
+};
+
+export const useCourse = (): Course => {
+  const chapters = courseData.chapters.map<Chapter>((chapter) => {
+    const lessons = chapter.lessons.map<Lesson>((lesson) => ({
+      ...lesson,
+      path: `/course/chapter/${chapter.slug}/lesson/${lesson.slug}`,
+    }));
+    return {
+      ...chapter,
+      lessons,
+    };
+  });
+
   return {
     ...courseData,
-    chapters: courseData.chapters.map((chapter) => ({
-      ...chapter,
-      lessons: chapter.lessons.map((lesson) => ({
-        ...lesson,
-        path: `/course/chapter/${chapter.slug}/lesson/${lesson.slug}`,
-      })),
-    })),
+    title: courseData.title,
+    chapters,
   };
 };
