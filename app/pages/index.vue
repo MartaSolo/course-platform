9f9bade4864876b86de3fba3582f0d39abc42c7b
MartaSołomacha <marciamarcia@o2.pl>
2026-05-07 22:18:30 +0200

    feat/add-middleware-and-login-page (#3)
    * Replace route validation with inline middleware
    
    * Add login page and refactor index page

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,5 +1,14 @@
+<script setup lang="ts">
+const { title } = useCourse();
+
+const { firstLesson } = useFirstLesson();
+</script>
+
 <template>
-  <div>
-    <h1>Hello, world!</h1>
+  <div class="prose w-full max-w-2xl h-9">
+    <h1>Welcome to {{ title }}</h1>
+    <NuxtLink :to="firstLesson" class="underline font-medium"
+      >Go to first lesson</NuxtLink
+    >
   </div>
 </template>
