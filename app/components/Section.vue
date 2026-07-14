03cd836f0f963e3fe1b5389020c60d81bc6c5859
Marta Solomacha <marciamarcia@o2.pl>
2026-07-14 13:42:25 +0200

    Update homepage components and assets

--- app/components/Section.vue
+++ app/components/Section.vue
@@ -10,7 +10,7 @@ defineOptions({
 
 <template>
   <div class="section bg-white px-8 py-12 lg:p-24 flex flex-col items-center">
-    <div class="w-full">
+    <div class="w-full max-w-7xl mx-auto">
       <h2 v-if="title" class="text-2xl lg:text-4xl font-bold pb-8 text-center">
         {{ title }}
       </h2>
